68df83b5ba25eeb2309d8a3b3f377456d2e49684
Pushpit <pushpit07@gmail.com>
2021-05-08 19:32:17 +0530

    Updates

---
+++ precache-manifest.d11d6641306c4664348572696d65c55e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "bcd7eff5da90c9fb2062",
+    "url": "/EthSwap/static/css/main.6db07fc9.chunk.css"
+  },
+  {
+    "revision": "bcd7eff5da90c9fb2062",
+    "url": "/EthSwap/static/js/main.bcd7eff5.chunk.js"
+  },
+  {
+    "revision": "eb34fc8824b23fa7dbbd",
+    "url": "/EthSwap/static/css/1.9a365b54.chunk.css"
+  },
+  {
+    "revision": "eb34fc8824b23fa7dbbd",
+    "url": "/EthSwap/static/js/1.eb34fc88.chunk.js"
+  },
+  {
+    "revision": "23f7ea5a753bbd83b86f",
+    "url": "/EthSwap/static/js/runtime~main.23f7ea5a.js"
+  },
+  {
+    "revision": "4a5b480da6bf5553ec7c73eadc200093",
+    "url": "/EthSwap/static/media/token-logo.4a5b480d.png"
+  },
+  {
+    "revision": "cb2396120039195c7ca5bd9ecf18b203",
+    "url": "/EthSwap/static/media/background.cb239612.png"
+  },
+  {
+    "revision": "b1a3302bfe0b4d4379f6bba5ba7a086f",
+    "url": "/EthSwap/index.html"
+  }
+];
